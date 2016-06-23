bfa73b0a87b46064d187c7d95e21c9731e06f218
Bid Sharma <bid@Bids-MacBook-Pro.local>
2016-06-23 23:23:53 +0545

    First commit

---
+++ visaDateNotify.js
@@ -0,0 +1,50 @@
+
+
+function getFirstAvailable(){
+	var months = [ "January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December" ];
+
+	var leftPaneDivs = document.getElementsByClassName('leftPanelText');
+	var textArray = leftPaneDivs[0].innerText.split(" ");
+	textArray = textArray.reverse();
+
+	var year = parseInt(textArray[0].replace('.',''));
+	var day = parseInt(textArray[1]);
+	var month = months.indexOf(textArray[2]);
+
+	return new Date(year,month,day)
+}
+
+
+
+function takeDateInput() {
+
+	var year = parseInt(prompt("Enter year"));
+	var month = parseInt(prompt("Enter month"));
+	var day = parseInt(prompt("Enter day"));
+
+	var yourVisaDate = new Date(year, month, day);
+
+	return yourVisaDate
+
+}
+
+function main() {
+
+	yourVisaDate = takeDateInput();
+	var id = setInterval(compareFunction,10000);
+
+	function compareFunction(){
+		firstAvailableDate = getFirstAvailable();
+
+		if(firstAvailableDate<yourVisaDate){
+			alert("Another date is available : "+firstAvailableDate);
+			clearInterval(id);
+
+		}else{
+			console.log("Watching for latest date");
+		}
+	}
+}
+
+main()
